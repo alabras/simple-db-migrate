e284d5f32f6fb0aae9819d1856fdef7bfd21f947
Alejandro Labra <alejandro.labra@thoughtworks.com>
2018-12-18 18:32:36 -0300

    add pyodbc driver for mssql by default.

--- simple_db_migrate/mssql.py
+++ simple_db_migrate/mssql.py
@@ -1,3 +1,4 @@
+import pyodbc 
 from .core import Migration
 from .core.exceptions import MigrationException
 from .helpers import Utils
@@ -8,7 +9,7 @@ class MSSQL(object):
         self.__mssql_script_encoding = config.get("database_script_encoding", "utf8")
         self.__mssql_encoding = config.get("database_encoding", "utf8")
         self.__mssql_host = config.get("database_host")
-        self.__mssql_port = config.get("database_port", 1433)
+        self.__mssql_port = config.get("database_port", "1433")
         self.__mssql_user = config.get("database_user")
         self.__mssql_passwd = config.get("database_password")
         self.__mssql_db = config.get("database_name")
@@ -16,8 +17,7 @@ class MSSQL(object):
 
         self.__mssql_driver = mssql_driver
         if not mssql_driver:
-            import _mssql
-            self.__mssql_driver = _mssql
+            self.__mssql_driver = pyodbc
 
         if config.get("drop_db_first"):
             self._drop_database()
@@ -27,31 +27,34 @@ class MSSQL(object):
 
     def __mssql_connect(self, connect_using_database_name=True):
         try:
-            conn = self.__mssql_driver.connect(server=self.__mssql_host, port=self.__mssql_port, user=self.__mssql_user, password=self.__mssql_passwd, charset=self.__mssql_encoding)
             if connect_using_database_name:
-                conn.select_db(self.__mssql_db)
+                conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.__mssql_host+';PORT=1433;DATABASE='+self.__mssql_db+';UID='+self.__mssql_user+';PWD='+ self.__mssql_passwd)
+            else:
+                conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.__mssql_host+';PORT=1433;DATABASE=master;UID='+self.__mssql_user+';PWD='+ self.__mssql_passwd)
             return conn
         except Exception as e:
             raise Exception("could not connect to database: %s" % e)
 
     def __execute(self, sql, execution_log=None):
         db = self.__mssql_connect()
+        cursor = db.cursor()
+        db.autocommit = False
         curr_statement = None
         try:
             statments = MSSQL._parse_sql_statements(sql)
             if len(sql.strip(' \t\n\r')) != 0 and len(statments) == 0:
                 raise Exception("invalid sql syntax '%s'" % Utils.encode(sql, "utf-8"))
-
+            
             for statement in statments:
                 curr_statement = statement
-                db.execute_non_query(statement)
-                affected_rows = db.rows_affected
+                affected_rows = cursor.execute(statement).rowcount
+                cursor.commit()
                 if execution_log:
                     execution_log("%s\n-- %d row(s) affected\n" % (statement, affected_rows and int(affected_rows) or 0))
         except Exception as e:
-            db.cancel()
             raise MigrationException("error executing migration: %s" % e, curr_statement)
         finally:
+            cursor.close()
             db.close()
 
     @classmethod
@@ -81,16 +84,22 @@ class MSSQL(object):
 
     def _drop_database(self):
         db = self.__mssql_connect(False)
+        cursor = db.cursor()
         try:
-            db.execute_non_query("if exists ( select 1 from sysdatabases where name = '%s' ) drop database %s;" % (self.__mssql_db, self.__mssql_db))
+            cursor.execute("if exists ( select 1 from sysdatabases where name = '%s' ) drop database %s;" % (self.__mssql_db, self.__mssql_db))
+            cursor.commit()
         except Exception as e:
             raise Exception("can't drop database '%s'; \n%s" % (self.__mssql_db, str(e)))
         finally:
+            cursor.close()
             db.close()
 
     def _create_database_if_not_exists(self):
         db = self.__mssql_connect(False)
-        db.execute_non_query("if not exists ( select 1 from sysdatabases where name = '%s' ) create database %s;" % (self.__mssql_db, self.__mssql_db))
+        cursor = db.cursor()
+        cursor.execute("if not exists ( select 1 from sysdatabases where name = '%s' ) create database %s;" % (self.__mssql_db, self.__mssql_db))
+        cursor.commit()
+        cursor.close()
         db.close()
 
     def _create_version_table_if_not_exists(self):
@@ -100,7 +109,9 @@ class MSSQL(object):
 
         # check if there is a register there
         db = self.__mssql_connect()
-        count = db.execute_scalar("select count(*) from %s;" % self.__version_table)
+        cursor = db.cursor()
+        count = db.execute("select count(*) from %s;" % self.__version_table).fetchone()
+        cursor.close()
         db.close()
 
         # if there is not a version register, insert one
@@ -114,24 +125,27 @@ class MSSQL(object):
 
         if up:
             # moving up and storing history
-            sql = "insert into %s (version, label, name, sql_up, sql_down) values (%%s, %%s, %%s, %%s, %%s);" % (self.__version_table)
+            sql = "insert into %s (version, label, name, sql_up, sql_down) values (?, ?, ?, ?, ?);" % (self.__version_table)
             params.append(label_version)
             params.append(migration_file_name)
             params.append(sql_up and Utils.encode(sql_up, self.__mssql_script_encoding) or "")
             params.append(sql_down and Utils.encode(sql_down, self.__mssql_script_encoding) or "")
         else:
             # moving down and deleting from history
-            sql = "delete from %s where version = %%s;" % (self.__version_table)
+            sql = "delete from %s where version = ?;" % (self.__version_table)
 
+        print (sql)
         db = self.__mssql_connect()
+        cursor = db.cursor()
         try:
-            db.execute_non_query(Utils.encode(sql, self.__mssql_script_encoding), tuple(params))
+            cursor.execute(Utils.encode(sql, self.__mssql_script_encoding), tuple(params))
+            cursor.commit()
             if execution_log:
                 execution_log("migration %s registered\n" % (migration_file_name))
         except Exception as e:
-            db.cancel()
             raise MigrationException("error logging migration: %s" % e, migration_file_name)
         finally:
+            cursor.close()
             db.close()
 
     def change(self, sql, new_db_version, migration_file_name, sql_up, sql_down, up=True, execution_log=None, label_version=None):
@@ -140,40 +154,47 @@ class MSSQL(object):
 
     def get_current_schema_version(self):
         db = self.__mssql_connect()
-        version = db.execute_scalar("select top 1 version from %s order by id desc" % self.__version_table) or 0
+        cursor = db.cursor()
+        version = cursor.execute("select top 1 version from %s order by id desc" % self.__version_table).fetchone() or 0
+        cursor.close()
         db.close()
         return version
 
     def get_all_schema_versions(self):
         versions = []
         db = self.__mssql_connect()
-        db.execute_query("select version from %s order by id;" % self.__version_table)
-        all_versions = db
+        cursor = db.cursor()
+        all_versions = cursor.execute("select version from %s order by id;" % self.__version_table).fetchall()
         for version in all_versions:
             versions.append(version['version'])
+        cursor.close()
         db.close()
         versions.sort()
         return versions
 
     def get_version_id_from_version_number(self, version):
         db = self.__mssql_connect()
-        result = db.execute_row("select id from %s where version = '%s' order by id desc;" % (self.__version_table, version))
+        cursor = db.cursor()
+        result = cursor.execute("select id from %s where version = '%s' order by id desc;" % (self.__version_table, version)).fetchone()
         _id = result and int(result['id']) or None
+        cursor.close()
         db.close()
         return _id
 
     def get_version_number_from_label(self, label):
         db = self.__mssql_connect()
-        result = db.execute_row("select version from %s where label = '%s' order by id desc" % (self.__version_table, label))
+        cursor = db.cursor()
+        result = cursor.execute("select version from %s where label = '%s' order by id desc" % (self.__version_table, label)).fetchone()
         version = result and result['version'] or None
+        cursor.close()
         db.close()
         return version
 
     def get_all_schema_migrations(self):
         migrations = []
         db = self.__mssql_connect()
-        db.execute_query("select id, version, label, name, cast(sql_up as text) as sql_up, cast(sql_down as text) as sql_down from %s order by id;" % self.__version_table)
-        all_migrations = db
+        cursor = db.cursor()
+        all_migrations = cursor.execute("select id, version, label, name, cast(sql_up as text) as sql_up, cast(sql_down as text) as sql_down from %s order by id;" % self.__version_table).fetchall()
         for migration_db in all_migrations:
             migration = Migration(id = int(migration_db['id']),
                                   version = migration_db['version'] and str(migration_db['version']) or None,
@@ -182,5 +203,6 @@ class MSSQL(object):
                                   sql_up = Migration.ensure_sql_unicode(migration_db['sql_up'], self.__mssql_script_encoding),
                                   sql_down = Migration.ensure_sql_unicode(migration_db['sql_down'], self.__mssql_script_encoding))
             migrations.append(migration)
+        cursor.close()
         db.close()
         return migrations
